9f770223f7b59170d68d837129d39eeffb4a316c
jfcoker <jfc20@ic.ac.uk>
2025-06-23 16:11:08 +0100

    Read command line argument

--- carbon/__main__.py
+++ carbon/__main__.py
@@ -1,1 +1,12 @@
 """The entry point for the carbon program."""
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        jobID = sys.argv[1]
+    except IndexError:
+        print("Usage: poetry run python -m carbon <jobID>")
+        raise
+
+    print(f"Your jobID is: {jobID}")
