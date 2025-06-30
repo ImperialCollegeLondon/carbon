60e7d3321d20b003e0d7edbbe8e5f432800ce3b0
jfcoker <jfc20@ic.ac.uk>
2025-06-30 14:47:11 +0100

    Use jobID instead of specifying resources

--- carbon/__main__.py
+++ carbon/__main__.py
@@ -2,44 +2,31 @@
 
 if __name__ == "__main__":
     import sys
-    from datetime import datetime
 
     from carbon.energy import Energy
     from carbon.intensity import CarbonIntensity
+    from carbon.job import Job
 
     try:
-        date_arg = sys.argv[1]
-        walltime_arg = sys.argv[2]
-        ncores_arg = sys.argv[3]
+        id = sys.argv[1]
     except IndexError:
-        print(
-            "Usage: poetry run python -m carbon "
-            "<date_arg / date> <walltime / hours> <ncores>"
-        )
+        print("Usage: poetry run python -m carbon <jobID>")
         raise
 
-    when = datetime.fromisoformat(date_arg)
-    walltime = float(walltime_arg)
-    ncores = int(ncores_arg)
+    # Get the job data
+    job = Job(id)
 
-    # Create a CarbonIntensity object with the specified time
-    carbon_intensity = CarbonIntensity(when)
-
-    # Create an Energy object with the specified number of CPU cores and walltime
-    energy = Energy(ncores, walltime)
-
-    # Fetch carbon intensity data
+    # Fetch carbon intensity at job startime time
+    carbon_intensity = CarbonIntensity(job._starttime)
     intensity = carbon_intensity.fetch()
 
     # Calculate energy consumption
+    energy = Energy(job._cputime)
     energy_consumed = energy.calculate()
 
     # Calculate emissions
     emissions = intensity * energy_consumed
 
-    print(f"Carbon intensity for datetime {when} is {intensity} gCO2/kWh")
-    print(
-        f"Energy consumed for {walltime} hours on {ncores} cores "
-        f"is {energy_consumed} kWh"
-    )
+    print(f"Carbon intensity for {job._starttime} is {intensity} gCO2/kWh")
+    print(f"Energy consumed for {job._cputime} CPU-hoursis {energy_consumed} kWh")
     print(f"Estimated emissions is {round(emissions)} gCO2")
