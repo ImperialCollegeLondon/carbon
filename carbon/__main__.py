c92ebc9a403094957cbd25dcdcf8d7fd8a261be7
jfcoker <jfc20@ic.ac.uk>
2025-06-30 15:02:14 +0100

    Change formatting of output string

--- carbon/__main__.py
+++ carbon/__main__.py
@@ -28,5 +28,7 @@ if __name__ == "__main__":
     emissions = intensity * energy_consumed
 
     print(f"Carbon intensity for {job._starttime} is {intensity} gCO2/kWh")
-    print(f"Energy consumed for {job._cputime} CPU-hoursis {energy_consumed} kWh")
+    print(
+        f"Energy consumed for {job._cputime:.2f} CPU-hours is {energy_consumed:.2f} kWh"
+    )
     print(f"Estimated emissions is {round(emissions)} gCO2")
