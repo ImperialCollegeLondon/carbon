28719eb67b22f866f7908ab5ed1466109f9f9ea9
Minjae Kang <66543161+theminjae@users.noreply.github.com>
2025-10-28 03:12:46 +0000

    Update test for generic region ID (#159)

--- tests/test_intensity.py
+++ tests/test_intensity.py
@@ -1,17 +1,25 @@
 """Unit tests for the CarbonIntensity class."""
 
 from datetime import datetime
+from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
+import yaml
 
+from carbon.clusterconfig import ClusterConfig
 from carbon.intensity import CarbonIntensity
+
+with open(Path("clusters/dummy.yaml")) as f:
+    TEST_CONFIG = yaml.safe_load(f)
+config = ClusterConfig(**TEST_CONFIG)
+region_id = config.region_id
 
 
 def test_carbon_intensity_init() -> None:
     """Test CarbonIntensity initialization."""
     dt = datetime(2025, 8, 21, 12, 0, 0)
-    ci = CarbonIntensity(dt, region_id=13)
+    ci = CarbonIntensity(dt, region_id)
     assert ci._stime.startswith("2025-08-21T12:00")
     assert ci._stime_plus.startswith("2025-08-21T12:30")
 
@@ -31,5 +39,5 @@ def test_carbon_intensity_fetch(mock_response) -> None:
     """Test CarbonIntensity.fetch() with mocked API response."""
     with patch("requests.get", return_value=mock_response):
         dt = datetime(2025, 8, 21, 12, 0, 0)
-        ci = CarbonIntensity(dt, region_id=13)
+        ci = CarbonIntensity(dt, region_id)
         assert ci.fetch() == 120.0
